ea16d57153ab537da1b4b41c2eac4e0323c59445
BreezeWhite <miyashita2010@tuta.io>
2025-06-11 11:32:05 +0800

    Add draft crate doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+//! # Bitfinex API Library & CLI Tool
+//!
+//! A fully asynchronous Rust library and CLI for accessing the Bitfinex V2 API.
+//!
+//! ## Highlights
+//! - Async-first: Non-blocking, efficient API calls.
+//! - CLI included: Query Bitfinex from your terminal.
+//! - Lightweight: Minimal dependencies.
+//! - Auto-retry: Handles "Nonce: small" errors automatically.
+//!
+//! ## Example
+//! ```rust
+//! use bfx::client::Client;
+//! async fn run() {
+//!     let client = Client::new("".into(), "".into());
+//!     let ticker = client.request_trading_ticker("tBTCUSD").await.unwrap();
+//!     println!("{:?}", ticker);
+//! }
+//! ```
+//!
+//! ## Feature flags
+//! - `cli` - Only used when you want to build and run as CLI.
 // #[cfg(feature = "cli")]
 pub mod cli;
 pub mod client;
